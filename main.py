362e47f3647393bad7346a3f51443ea95845c56b
prasannakumarj-18 <saijyothula18@gmail.com>
2026-09-14 19:45:42 +0530

    Move Oracle password to environment variable

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import json
 # -------------------------
 connection = oracledb.connect(
     user="system",
-    password="1234",
+    password=os.getenv("1234"),
     dsn="localhost:1521/FREE"
 )
 
